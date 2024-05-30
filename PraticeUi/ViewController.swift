69abffe18e933449fa5f1de631a9b2df58ce4045
ShinHB <shinhp3@naver.com>
2024-05-30 18:36:08 +0900

    Initial Commit

---
+++ PraticeUi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PraticeUi
+//
+//  Created by 신효범 on 5/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
